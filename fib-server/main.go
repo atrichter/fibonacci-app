171da51af4b26d0fae18381af0dda88ae02f4fd7
Andrew Richter <andrewrichter22@gmail.com>
2022-03-01 12:20:27 -0800

    fib-server cleanup

--- fib-server/main.go
+++ fib-server/main.go
@@ -1,67 +1,68 @@
 package main
 
 import (
-  "fmt"
-  "github.com/julienschmidt/httprouter"
-  "net/http"
-  "log"
-  "strconv"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func enableCors(w *http.ResponseWriter) {
-  (*w).Header().Set("Access-Control-Allow-Origin", "*")
-  (*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-  fmt.Fprint(w, "Welcome!\n")
+	fmt.Fprint(w, "Welcome!\n")
 }
 
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-  fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
 func Fibonacci(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-  // add headers to allow cors
-  enableCors(&w)
+	// add headers to allow cors
+	enableCors(&w)
 
-  // only calculate fibonacci if numDigits converts to an integer
-  if numDigits, err := strconv.Atoi(ps.ByName("numDigits")); err == nil {
-    num1 := int64(0)
-    num2 := int64(1)
-    nextNum := int64(0)
+	// only calculate fibonacci if numDigits converts to an integer
+	if numDigits, err := strconv.Atoi(ps.ByName("numDigits")); err == nil {
+		num1 := int64(0)
+		num2 := int64(1)
+		nextNum := int64(0)
 
-    for i := 0; i < numDigits; i++ {
-      if i == 0 {
-        fmt.Fprintf(w, "%d", num1)
-        continue
-      }
+		for i := 0; i < numDigits; i++ {
+			if i == 0 {
+				fmt.Fprintf(w, "%d", num1)
+				continue
+			}
 
-      if i == 1 {
-        fmt.Fprintf(w, ", %d", num2)
-        continue
-      }
+			if i == 1 {
+				fmt.Fprintf(w, ", %d", num2)
+				continue
+			}
 
-      nextNum = num1 + num2
-      num1 = num2
-      num2 = nextNum
+			nextNum = num1 + num2
+			num1 = num2
+			num2 = nextNum
 
-      // check for integer overflow
-      if nextNum >= 0 {
-        fmt.Fprintf(w, ", %d", nextNum)
-        continue;
-      }
-      
-      fmt.Fprintf(w, ", ... (too large)")
-      break;
-    }
-  }
+			// check for integer overflow
+			if nextNum >= 0 {
+				fmt.Fprintf(w, ", %d", nextNum)
+				continue
+			}
+
+			fmt.Fprintf(w, ", ... (too large)")
+			break
+		}
+	}
 }
 
 func main() {
-  router := httprouter.New()
-  router.GET("/api", Index)
-  router.GET("/api/hello/:name", Hello)
-  router.GET("/api/fibonacci/:numDigits", Fibonacci)
-  log.Fatal(http.ListenAndServe(":8080", router))
+	router := httprouter.New()
+	router.GET("/api", Index)
+	router.GET("/api/hello/:name", Hello)
+	router.GET("/api/fibonacci/:numDigits", Fibonacci)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
